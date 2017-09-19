2c4a930c85180fe402015637869e3858878e940e
SashaKoroleva <s.sashakoroleva.s@gmail.com>
2017-09-19 22:35:59 +0300

    begin,head,tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,41 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+      var node = new Node(data);
 
-    head() {}
+      if(this.length == 0){
+        this.head = node;
+        this.tail = node;
+      }
+      else{
+        this.tail.next = node;
+        node.previous = this.tail;
+        this.tail = node;
+      }
+      this.length++;
+      return this;
+    }
 
-    tail() {}
+    head() {
+     if(!this.head){
+       return null;
+     }
+     return this.head.data;
+    }
+
+    tail() {
+      if(!this.tail){
+        return null;
+      }
+      return this.tail.data;
+    }
 
     at(index) {}
 
